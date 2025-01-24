683989305c6d7a74aae186dca618fdba131961d3
LEESY02 <e1121352@u.nus.edu>
2025-01-24 11:03:05 +0800

    Added Seung Yoon

---
+++ src/main/java/SeungYoon.java
@@ -0,0 +1,12 @@
+public class SeungYoon {
+    public static void main(String[] args) {
+        String message =
+        "____________________________________________________________\n" +
+        "Hello! I'm Seung Yoon\n" +
+        "What can I do for you?\n" +
+        "____________________________________________________________\n" +
+        "Bye. Hope to see you again soon!\n" +
+        "____________________________________________________________\n";
+        System.out.println(message);
+    }
+}

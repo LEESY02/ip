d1faab1ed24aaff93ffda5bb4478d3b08c4be662
LEESY02 <e1121352@u.nus.edu>
2025-01-26 12:12:41 +0800

    Added echo

--- src/main/java/SeungYoon.java
+++ src/main/java/SeungYoon.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class SeungYoon {
     public static void main(String[] args) {
         String message =
         "____________________________________________________________\n" +
         "Hello! I'm Seung Yoon\n" +
         "What can I do for you?\n" +
-        "____________________________________________________________\n" +
-        "Bye. Hope to see you again soon!\n" +
         "____________________________________________________________\n";
+        System.out.println(message);
+
+        Scanner sc = new Scanner(System.in);
+        String nextLine = sc.nextLine();
+        while (!nextLine.equals("bye")) {
+            System.out.println(
+                "\t____________________________________________________________\n" +
+                "\t" + nextLine + "\n" +
+                "\t____________________________________________________________\n");
+            nextLine = sc.nextLine();
+        }
+
+        message =
+            "\t____________________________________________________________\n" +
+            "\tBye. Hope to see you again soon! (づ ᴗ _ᴗ)づ♡ ඞSUSSY\n" +
+            "\t____________________________________________________________\n";
         System.out.println(message);
     }
 }
